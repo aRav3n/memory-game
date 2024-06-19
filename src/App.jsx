67fe8717e421caf0811b8fc0023eca499f63b1dd
aRav3n <24660772+aRav3n@users.noreply.github.com>
2024-06-19 14:15:33 -0600

    build: write pseudo-code for game function

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
+
+// App title heading as well as footer display
+
+// Form displays for user to input their name and select a difficulty level
+
+// Pull X number of images from an API with useEffect where X is determined by selected difficulty level. X = [6 (easy), 9 (medium), or 12 (hard)] cards.
+
+// Reusable function placeCardsAtRandom() to place images at random spots in an array
+
+// Brief description of how to play appears on screen. Remember to have a highly visible X button to close it.
+
+// RAM css grid appears displaying all cards placed in random positions via placeCardsAtRandom().
+
+// h2 displaying both current score and best score
+
+// When user clicks a card it gets added to an array of clicked cards via useState()
+
+// Click also triggers another placeCardsAtRandom(). If successful then current score = current score++
+
+// Round ends when user has selected all cards and wins or accidentally selects a card twice and loses. Either way the top score will go into the h2 that displays best score
+
+// overlay that appears when a round is over
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return <></>;
 }
 
-export default App
+export default App;
